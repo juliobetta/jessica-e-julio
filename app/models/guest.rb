0a688ecdd3c9f52c6604c529f12c98fd841b636a
Julio Betta <juliobetta@gmail.com>
2014-03-04 18:08:46 -0300

    add email validation

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,8 @@
 class Guest < ActiveRecord::Base
-  validates_presence_of   :code, :email, :name, :status
+  validates_presence_of   :code, :name, :status
   validates_uniqueness_of :email, :code
   validates_inclusion_of  :status, :in => [:confirmed, :not_confirmed, :unconfirmed]
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   attr_accessible :code, :confirmed_aggregates, :email, :name, :possible_aggregates, :status
 
