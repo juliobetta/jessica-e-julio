acafb67202b490d9b24c446be8fc34fed38b17f9
Julio Betta <juliobetta@gmail.com>
2014-07-11 23:39:47 -0300

    change days left message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,14 @@ class HomeController < ApplicationController
     @guest  = Guest.new(code: '')
     @photos = Photo.all
 
-    @days_left = begin
+    days_left = begin
       now    = DateTime.now
       future = DateTime.new(2014, 07, 12)
 
       (future.to_date - now.to_date).to_i
     end
+
+    @status_message = days_left == 0 ? 'É Hoje!!!' : ( days_left == 1 ? 'Falta 1 dia' : "Faltam #{days_left} dias")
   end
 
   def gallery
